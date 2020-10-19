874b7a3cb242689b8833b1db171040e44c52c4d1
InfoExtract <infoextractpvtltd@gmail.com>
2020-10-19 21:32:47 +0530

    apis

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Test Page</title>
+	<script type="text/javascript" src="js/jquery-3.5.1.min.js"></script>
+</head>
+<body>
+	<style type="text/css">
+		#search{
+			width: 10%;
+			background-color: red;
+		}
+		button{
+			width: 60px;
+			height: 20px;
+		}
+	</style>
+	<input type="tex" id="search" name="txtSearch">
+	<button id="btnSave">Save</button>
+	<div id="data"></div>
+</body>
+</html>
